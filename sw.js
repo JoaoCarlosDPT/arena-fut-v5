171a52b401bb7ba4a0d329b4dfe04e6952f4c8c4
JoaoCarlosDPT <joaocdpt@gmail.com>
2026-04-24 15:00:23 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+{
+  "name": "Arena Fut PRO",
+  "short_name": "ArenaFut",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#0c0e11",
+  "theme_color": "#0c0e11",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
