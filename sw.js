caaf92c8c30940a411e98d0dd826d16f6fedc433
JoaoCarlosDPT <joaocdpt@gmail.com>
2026-04-24 18:18:50 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-{
-  "name": "Arena Fut PRO",
-  "short_name": "ArenaFut",
-  "start_url": "./index.html",
-  "display": "standalone",
-  "background_color": "#0c0e11",
-  "theme_color": "#0c0e11",
-  "icons": [
-    {
-      "src": "icon-192.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "icon-512.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ]
-}
+const CACHE_NAME = 'arena-fut-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
